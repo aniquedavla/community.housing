5da5608683761dfe4de12e2562f65f1cfc4f06f6
Anique Davla <aniquedavla@gmail.com>
2020-03-01 15:09:11 -0800

    anique latest

---
+++ src/HomePage/SearchBar.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import SearchBar from "@material-ui/core";
+
+// const SearchBar = () => (
+//     <SearchBar
+//         dataSource={state.dataSource}
+//         onChange={(value) => setState({dataSource: [ value, value+value, value+value+value]})}
+//         onRequestSearch={() => console.log('onRequestSearch')}
+//         style={{
+//         margin: '0 auto',
+//         maxWidth: 800
+//         }}
+//     />
+// );
+
+// export default SearchBar;
+
